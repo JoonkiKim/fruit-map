e400144b8314344dd1ba0f860a9bcab6d8324358
JoonkiKim <qkjk1508@gmail.com>
2024-12-30 17:30:37 +0900

    글로벌인포가 문제였는지 아니면 캐시무효화가 문제였는지 테스트를 위해 확인

--- client/src/components/commons/layout/header/index.tsx
+++ client/src/components/commons/layout/header/index.tsx
@@ -1,9 +1,9 @@
 // import { useRouter } from "next/router";
 
 import Link from "next/link";
-import { useState } from "react";
-import { useRecoilState } from "recoil";
-import { loggedInCheck } from "../../../../commons/stores";
+import { useEffect, useState } from "react";
+import { useRecoilState, useRecoilValueLoadable } from "recoil";
+import { loggedInCheck, marketinfoGlobal } from "../../../../commons/stores";
 import { auth } from "../../../../commons/libraries/firebase_fruitmap";
 // import { browserSessionPersistence } from "firebase/auth";
 // import { ModalAlert } from "../../../units/fruitsmap/map/map.style";
@@ -24,7 +24,7 @@ export default function LayoutHeader() {
   // const router = useRouter();
 
   const [isLoggedIn, setIsLoggedIn] = useRecoilState(loggedInCheck);
-  // const marketinfo = useRecoilValueLoadable(marketinfoGlobal);
+  const marketinfo = useRecoilValueLoadable(marketinfoGlobal);
 
   const [isModalAlertOpen, setIsModalAlertOpen] = useState(false);
 
@@ -35,12 +35,12 @@ export default function LayoutHeader() {
   const [randomIndex, setRandomIndex] = useState<number>(0); // 초기 값 설정
   console.log("테스트를 위해 모두 되돌림");
 
-  // useEffect(() => {
-  //   if (marketinfo.state === "hasValue" && marketinfo.contents.length > 0) {
-  //     // 클라이언트에서만 랜덤 인덱스를 설정
-  //     setRandomIndex(Math.floor(Math.random() * marketinfo.contents.length));
-  //   }
-  // }, [marketinfo]);
+  useEffect(() => {
+    if (marketinfo.state === "hasValue" && marketinfo.contents.length > 0) {
+      // 클라이언트에서만 랜덤 인덱스를 설정
+      setRandomIndex(Math.floor(Math.random() * marketinfo.contents.length));
+    }
+  }, [marketinfo]);
 
   return (
     <Wrapper>
@@ -57,13 +57,13 @@ export default function LayoutHeader() {
 
         {isLoggedIn ? (
           <>
-            {/* <RecommendWrapper>
+            <RecommendWrapper>
               {marketinfo.state === "hasValue" && marketinfo.contents.length > 0
                 ? randomIndex !== null
                   ? `오늘은 ${marketinfo.contents[randomIndex]?.name} ${marketinfo.contents[randomIndex]?.menu} 어때요?`
                   : "추천 과일을 찾는 중입니다."
                 : "추천 과일을 찾는 중입니다."}
-            </RecommendWrapper> */}
+            </RecommendWrapper>
             <HeaderRightWrapper>
               <LogInCheckWrapper>
                 {auth?.currentUser?.displayName}님 환영합니다!
